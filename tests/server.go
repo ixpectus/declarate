399ce1d163fd5eddc7ef3b672ac5f9309f77a594
ixpectus <ixpectus@gmail.com>
2024-07-29 13:16:24 +0300

    check polling with list

--- tests/server.go
+++ tests/server.go
@@ -55,6 +55,27 @@ func tomHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "I can't do that.")
 	}
 }
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Date", defaultDate)
+	switch r.Method {
+	case "GET":
+		// Just send out the JSON version of 'tom'
+		j, _ := json.Marshal(tom)
+		w.Write(j)
+	case "POST":
+		// Decode the JSON in the body and overwrite 'tom' with it
+		d := json.NewDecoder(r.Body)
+		p := &person{}
+		err := d.Decode(p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		tom = p
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "I can't do that.")
+	}
+}
 
 const defaultDate = "Sun, 16 Apr 2023 08:42:05 GMT"
 
@@ -86,9 +107,26 @@ func pollHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "I can't do that.")
 	}
 }
+func pollListHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Date", defaultDate)
+	switch r.Method {
+	case "GET":
+		// Just send out the JSON version of 'tom'
+		v := pollCounter.Add(1)
+		if v > 5 {
+			j := []byte(`{"databases":[],"extensions":[],"indexes":[],"roles":["postgres"],"schemas":[],"tables":[],"tablespaces":[]}`)
+			w.Write(j)
+			pollCounter.Store(0)
+		} else {
+			j := []byte(`{"databases":[],"extensions":[],"indexes":[],"roles":[],"schemas":[],"tables":[],"tablespaces":[]}`)
+			w.Write(j)
+		}
+	}
+}
 
 func Handle() {
 	http.HandleFunc("/tom", tomHandler)
 	http.HandleFunc("/poll", pollHandler)
+	http.HandleFunc("/pollList", pollListHandler)
 	http.ListenAndServe("127.0.0.1:8181", nil)
 }
